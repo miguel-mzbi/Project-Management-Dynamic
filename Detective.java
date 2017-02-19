2c5bcadd5f8f398988b864ebe2b737344bf648e9
GabrielAvilesR <gabrielaviles1995@gmail.com>
2017-02-19 11:37:26 -0600

    termine la funcion readFromFileInput donde se lee el archivo y se crea arreglo de int

--- Detective.java
+++ Detective.java
@@ -1,6 +1,3 @@
-import.lang.Object;
-import.lang.Throwable;
-import.lang.Exception;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -19,17 +16,33 @@ public class Detective{
 		BufferedReader br = new BufferedReader(new InputStreamReader(fis));
 
 		String line = br.readLine(); //aqui debe estar en la primera linea N. El numero de elementos que vendran en la siguiente linea
-		int arraySize = Integer.parseToInt(line);
+		arraySize = Integer.parseInt(line);
+		arrayClan = new int[arraySize];
+		line = br.readLine(); //aqui estan los elementos del clan
+		String[] splitLine = line.split(" ");
+		for(int i = 0; i < splitLine.length; i++){
+			arrayClan[i] = Integer.parseInt(splitLine[i]);
+		}
 
-		br.close;
-
-		return null;
+		br.close();
+		return arrayClan;
 	}
 	public static int[] getPotentialKillers(int[] clan){
-
+		return null;
 	}
 
-	public static void main(String[] main){
+	public static void main(String[] args){
+		if(args.length == 0){
+			System.out.println("Debe de llamar el archivo a probar como parametro al ejecutar el programa");
+		}else{
+			File in = new File(args[0]);
+			try{
+				int[] clan = readFromFileInput(in);
 
+			}catch(IOException err){
+				System.out.println(err.toString());
+			}
+			
+		}
 	}
 }
