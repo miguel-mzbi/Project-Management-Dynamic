35ca830cd0f40190bdc6d5978a9a8a7ad16a6f68
GabrielAvilesR <gabrielaviles1995@gmail.com>
2017-02-19 11:54:52 -0600

    Agregue la funcion getPotentialKillers que regresa un arreglo con la respuesta

--- Detective.java
+++ Detective.java
@@ -28,9 +28,29 @@ public class Detective{
 		return arrayClan;
 	}
 	public static int[] getPotentialKillers(int[] clan){
-		return null;
+		int[] reportTo = new int[clan.length]; //aqui guardare cuanta gente le reporta, la gente que tenga 0 sera regresada como respuesta
+		for(int i = 0; i < clan.length; i++){
+			if(clan[i] > 0){
+				reportTo[clan[i]-1]++;
+			}
+		}
+		int sizeOfResult = 0;
+		for(int i = 0; i < reportTo.length; i++){
+			if(reportTo[i] == 0){
+				sizeOfResult++;
+			}
+		}
+		int[] result = new int[sizeOfResult];
+		int j = 0;
+		for(int i = 0; i < reportTo.length;i++){
+			if(reportTo[i] == 0){
+				result[j++] = i+1;
+			}
+		}
+		
+		return result;
 	}
-
+	
 	public static void main(String[] args){
 		if(args.length == 0){
 			System.out.println("Debe de llamar el archivo a probar como parametro al ejecutar el programa");
@@ -38,7 +58,8 @@ public class Detective{
 			File in = new File(args[0]);
 			try{
 				int[] clan = readFromFileInput(in);
-
+				int[] potentialKillers = getPotentialKillers(clan);
+				
 			}catch(IOException err){
 				System.out.println(err.toString());
 			}
